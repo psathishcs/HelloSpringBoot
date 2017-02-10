167ae1d0300901d7aea9dcf0995f8190463b99de
Sathish Kumar Parthasarathy <p.sathish.cs@gmail.com>
2017-02-09 21:15:52 -0600

    fix

--- src/main/java/org/hello/boot/SpringBootWebApplication.java
+++ src/main/java/org/hello/boot/SpringBootWebApplication.java
@@ -2,9 +2,11 @@ package org.hello.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBootWebApplication {
 	public static void main(String args[]) throws Exception {
 		SpringApplication.run(SpringBootWebApplication.class, args);
